486338336d630afe88a760a6b3510214a7fe8c2e
daisie_local <daisieh@gmail.com>
2021-12-13 13:12:10 -0800

    first pass at ETL for candigv1

--- ingest.sh
+++ ingest.sh
@@ -8,6 +8,18 @@ cd samples
 python ../generate_genomic.py
 cd ..
 
+# prep for ETL to katsu:
+git clone https://github.com/CanDIG/medidata_mCode_ETL.git
+cd medidata_mCode_ETL
+git checkout daisieh/updates
+pip install -r requirements.txt
+cd ..
+
+# prep data in candigv1 format:
+python ../medidata_mCode_ETL/CSVConvert.py --input Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2.xlsx --mapping mappings/synthetic2candigv1/manifest.yml
+
+
+
 # ingest data into katsu
 docker cp mCode_ingest_scripts.json candigv2_chord-metadata_1:/shared
 python katsu_ingest.py mohccn mcode-synthetic mcode-synthetic http://0.0.0.0:8009 /shared/mCode_ingest_scripts.json mcodepacket
