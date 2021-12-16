cd11ab593264c27153caf8b292901cb9664284bc
daisie_local <daisieh@gmail.com>
2021-12-16 15:47:59 -0800

    mappings, more ingest

--- ingest.sh
+++ ingest.sh
@@ -17,9 +17,14 @@ cd ..
 
 # prep data in candigv1 format:
 python ../medidata_mCode_ETL/CSVConvert.py --input Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2.xlsx --mapping mappings/synthetic2candigv1/manifest.yml
+mv Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2_map.json /shared/Synthetic_Clinical_Data_2_map_v1.json
 
+# prep data in mcode format:
+python ../medidata_mCode_ETL/CSVConvert.py --input Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2.xlsx --mapping mappings/synthetic2mcode/manifest.yml
+mv Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2_map.json /shared/Synthetic_Clinical_Data_2_map_mcode.json
+
+# ingest data into candigv1
 
 
 # ingest data into katsu
-docker cp mCode_ingest_scripts.json candigv2_chord-metadata_1:/shared
-python katsu_ingest.py mohccn mcode-synthetic mcode-synthetic http://0.0.0.0:8009 /shared/mCode_ingest_scripts.json mcodepacket
+python katsu_ingest.py mohccn mcode-synthetic mcode-synthetic http://0.0.0.0:8009 /shared/Synthetic_Clinical_Data_2_map_mcode.json mcodepacket
