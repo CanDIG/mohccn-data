3ba23fff426e44c822c81b7b3da9b537541630d7
daisie_local <daisieh@gmail.com>
2022-01-04 22:06:08 -0800

    change name from medidata_mCode_ETL to clinical_ETL

--- ingest.sh
+++ ingest.sh
@@ -31,21 +31,21 @@ docker cp samples/compressed candigv2_chord-metadata_1:/shared/
 exit
 
 # prep for ETL to katsu:
-git clone https://github.com/CanDIG/medidata_mCode_ETL.git
-cd medidata_mCode_ETL
+git clone https://github.com/CanDIG/clinical_ETL.git
+cd clinical_ETL
 git checkout daisieh/updates
 pip install -r requirements.txt
 cd ..
 
 # prep data in mcode format:
-python ../medidata_mCode_ETL/CSVConvert.py --input Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2.xlsx --mapping mappings/synthetic2mcode/manifest.yml
+python ../clinical_ETL/CSVConvert.py --input Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2.xlsx --mapping mappings/synthetic2mcode/manifest.yml
 docker cp Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2_map.json candigv2_chord-metadata_1:/shared/Synthetic_Clinical_Data_2_map_mcode.json
 
 # ingest data into katsu
 python katsu_ingest.py mohccn mcode-synthetic mcode-synthetic http://0.0.0.0:8008 /shared/Synthetic_Clinical_Data_2_map_mcode.json mcodepacket
 
 # prep data in candigv1 format:
-python ../medidata_mCode_ETL/CSVConvert.py --input Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2.xlsx --mapping mappings/synthetic2candigv1/manifest.yml
+python ../clinical_ETL/CSVConvert.py --input Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2.xlsx --mapping mappings/synthetic2candigv1/manifest.yml
 docker cp Synthetic_Clinical+Genomic_data/Synthetic_Clinical_Data_2_map.json candigv2_chord-metadata_1:/shared/Synthetic_Clinical_Data_2_map_candigv1.json
 
 # ingest data into candigv1
